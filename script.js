73c5af83cc36826ec82e1ea60470a1f3c2201e42
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-13 12:46:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const container = document.getElementsByClassName("container");
+console.log(container)
+for(let i=1;i<=800;i++){
+	const square = document.createElement("p");
+	square.className = "square";
+	container[0].appendChild(square);
+}
